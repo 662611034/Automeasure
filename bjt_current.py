d45a83c7685322a007be1f25999c5636ce4e4d73
ripperDT <parkj@etri.re.kr>
2019-04-05 17:06:23 +0900

    tab to space

--- bjt_current.py
+++ bjt_current.py
@@ -13,33 +13,33 @@ result = [['VC']]
 
 vb = vb_start
 while vb < vb_stop + vb_step:
-	result[0].append(f'I_VB={round(vb, 1)}V')
-	vb += vb_step
+    result[0].append(f'I_VB={round(vb, 1)}V')
+    vb += vb_step
 
 vc = vc_start
 while vc < vc_stop + vc_step:
-	result.append([round(vc, 1)])
-	vc += vc_step
+    result.append([round(vc, 1)])
+    vc += vc_step
 
 vb = vb_start
 while vb < vb_stop + vb_step:
-	i = 1
-	vc = vc_start
-	while vc < vc_stop + vc_step:
-		# power.setVolt(vb, 1)
-		# power.setVolt(vc, 2)
-		# curr = power.readCurr(2)
-		curr = round(10*vb*vc, 1)
-		result[i].append(curr)
-		i +=1
-		vc += vc_step
-	vb += vb_step
+    i = 1
+    vc = vc_start
+    while vc < vc_stop + vc_step:
+        # power.setVolt(vb, 1)
+        # power.setVolt(vc, 2)
+        # curr = power.readCurr(2)
+        curr = round(10*vb*vc, 1)
+        result[i].append(curr)
+        i +=1
+        vc += vc_step
+    vb += vb_step
 
 with open('result.txt', 'w') as f:
-	for i in range(len(result)):
-		for j in range(len(result[i])):
-			f.write(str(result[i][j])+' ')
-		f.write('\n')
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            f.write(str(result[i][j])+' ')
+        f.write('\n')
 
 ###############################################
 
@@ -61,23 +61,23 @@ list_temp = []
 
 vc = vc_start
 while vc < vc_stop + vc_step:
-	list_temp.append([round(vc, 1)])
-	vc += vc_step
+    list_temp.append([round(vc, 1)])
+    vc += vc_step
 result['VC'] = list_temp
 result = result.set_index('VC')
 
 vb = vb_start
 while vb < vb_stop + vb_step:
-	vc = vc_start
-	list_temp=[]
-	while vc < vc_stop + vc_step:
-		# power.setVolt(vb, 1)
-		# power.setVolt(vc, 2)
-		# curr = power.readCurr(2)
-		curr = round(10*vb*vc, 1)
-		list_temp.append(curr)
-		vc += vc_step
-	result[f'VB={round(vb, 1)}V'] = list_temp
-	vb += vb_step
+    vc = vc_start
+    list_temp=[]
+    while vc < vc_stop + vc_step:
+        # power.setVolt(vb, 1)
+        # power.setVolt(vc, 2)
+        # curr = power.readCurr(2)
+        curr = round(10*vb*vc, 1)
+        list_temp.append(curr)
+        vc += vc_step
+    result[f'VB={round(vb, 1)}V'] = list_temp
+    vb += vb_step
 
 result.to_csv('result.csv')
