e16ee34b26f36bb2d34140f6e7b53d7d5d11a3dc
ripperDT <parkj@etri.re.kr>
2020-02-10 09:49:38 +0900

    fix ip

--- Ex4_socket/socket_idn.py
+++ Ex4_socket/socket_idn.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # タイムアウトを2秒に設定
 s.settimeout(2)
 # 測定器と接続
-s.connect(("192.168.1.246", 5025))
+s.connect(("192.168.0.11", 5025))
 
 s.send("*IDN?\n".encode())
 id = s.recv(64)
